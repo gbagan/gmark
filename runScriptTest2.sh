1265dc89198fa21558e5cb39fd37906809f60de1
wilcovl <wilco_1993@live.nl>
2016-10-27 10:25:07 +0200

    Update test script

---
+++ runScriptTest2.sh
@@ -0,0 +1,25 @@
+./src/test -c use-cases/test.xml -n 2500
+
+./src/test -c use-cases/test.xml -n 5000
+
+./src/test -c use-cases/test.xml -n 10000
+
+./src/test -c use-cases/test.xml -n 20000
+
+./src/test -c use-cases/test.xml -n 30000
+
+./src/test -c use-cases/test.xml -n 40000
+
+./src/test -c use-cases/test.xml -n 60000
+
+./src/test -c use-cases/test.xml -n 80000
+
+./src/test -c use-cases/test.xml -n 100000
+
+./src/test -c use-cases/test.xml -n 200000
+
+./src/test -c use-cases/test.xml -n 400000
+
+./src/test -c use-cases/test.xml -n 800000
+
+./src/test -c use-cases/test.xml -n 1000000
